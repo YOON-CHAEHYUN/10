a230e1598988236352680d0b3319d1176db3b2ed
YOON-CHAEHYUN <yunch0202@naver.com>
2022-11-08 17:10:53 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,17 +5,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
-	char str[30];
-	FILE*fp;
-	fp=fopen("sample.txt","w");
+	char c;
+	FILE*fp=NULL;
+	fp=fopen("sample.txt","r");
 	
-	for(i=0;i<3;i++){
-		printf("input a word: ");
-		scanf("%s", str);//문자열은 &필요없음 
-		fprintf(fp,"%s\n",str);
-		printf("\n");
-	}
+	while((c=fgetc(fp))!=EOF)//문자변수=fgetc(파일포인터) 
+		putchar(c);
+	
 	
 	fclose(fp);
 }
